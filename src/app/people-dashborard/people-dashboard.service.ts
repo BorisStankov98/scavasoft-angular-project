014635bf592af27852197975931ab2c5ad076c79
Boris Stankov <boris.stankovv@gmail.com>
2022-06-29 14:59:47 +0300

    test

--- src/app/people-dashborard/people-dashboard.service.ts
+++ src/app/people-dashborard/people-dashboard.service.ts
@@ -13,6 +13,7 @@ export class PeopleDashboardService {
             {name:'Stephan',age:40,isWorking:true,skills:null},
             {name:'Yosif',age:32,isWorking:false,skills:['wordpress','marketing']},
             {name:'Pesho',age:30,isWorking:true,skills:null},
+            {name:'Pesho',age:30,isWorking:true,skills:null},
           ];
     }
 
